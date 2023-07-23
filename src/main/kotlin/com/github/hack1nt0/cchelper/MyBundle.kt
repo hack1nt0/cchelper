64eca837515c3199dde4fd6491c676a3cc7fab2f
GitHub Action <action@github.com>
2023-07-23 12:23:59 +0000

    Template cleanup

--- src/main/kotlin/com/github/hack1nt0/cchelper/MyBundle.kt
+++ src/main/kotlin/com/github/hack1nt0/cchelper/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.hack1nt0.cchelper
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
--- src/main/kotlin/com/github/hack1nt0/cchelper/listeners/MyApplicationActivationListener.kt
+++ src/main/kotlin/com/github/hack1nt0/cchelper/listeners/MyApplicationActivationListener.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.hack1nt0.cchelper.listeners
 
 import com.intellij.openapi.application.ApplicationActivationListener
 import com.intellij.openapi.diagnostic.thisLogger
